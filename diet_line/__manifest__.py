0a5c1a9fd9ee7c0aae4ecb6551476d2082dd29d0
Humberto Arocha <hbto@vauxoo.com>
2018-07-12 14:36:54 -0400

    [IMP] Adding Modules to be used by Diet Line

---
+++ diet_line/__manifest__.py
@@ -0,0 +1,23 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Diet Line App',
+    'version': '11.0.1.0.0',
+    "author": "Vauxoo",
+    "license": "LGPL-3",
+    'category': 'Hidden',
+    'summary': 'Diet Line App for customizations',
+    'depends': [
+        'crm',
+        'account_invoicing',
+        'account_accountant',
+        'website',
+        'mass_mailing',
+        'marketing_automation',
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'auto_install': True,
+}
